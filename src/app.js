61a4b045d974d6ee6fbb338708274f369d81ca96
BoliViteri <baviteri@gmail.com>
2022-10-06 17:49:31 +0000

    domain generator work

--- src/app.js
+++ src/app.js
@@ -6,6 +6,17 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our", "this"];
+  let adj = ["great", "big", "grand"];
+  let noun = ["jogger", "racoon", "doctor", "school"];
+  let extension_end = [".net", ".com", ".UK"];
+  let pronounEntry = pronoun[Math.floor(Math.random() * 3)];
+  let adjEntry = adj[Math.floor(Math.random() * 3)];
+  let nounEntry = noun[Math.floor(Math.random() * 4)];
+  let extension_endEntry = extension_end[Math.floor(Math.random() * 3)];
+
+  let DomainGenerated =
+    pronounEntry + adjEntry + nounEntry + extension_endEntry;
+
+  document.getElementById("domainGenerated").innerText = DomainGenerated;
 };
